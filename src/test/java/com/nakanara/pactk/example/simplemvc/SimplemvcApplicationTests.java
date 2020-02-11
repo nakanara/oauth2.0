31865bf44ac59d26093cde605943af55a3570d31
nakanara <nakanara@naver.com>
2020-02-12 00:51:46 +0900

    add gitignore

---
+++ src/test/java/com/nakanara/pactk/example/simplemvc/SimplemvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nakanara.pactk.example.simplemvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplemvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
